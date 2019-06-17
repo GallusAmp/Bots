7836a29a98a58bb12874189cf4cbdba9b8938f87
GallusAmp <ariel.p.medrano@gmail.com>
2019-06-17 13:23:09 -0300

    Add files via upload
    Fixed various errors

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler 	              
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler                  
+import requests
 import re
 
-def url():
-	contents = requests.get('https://random.dog/woof.json').json()
-	image_url = contents['url']
-	return url
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+def get_url():
+    contents =  requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
 
 def image_url():
-	allowed_extension = ['jpg','jpeg','png']
-	file_extension = ''    
-	while file_extension not in allowed_extension:        
-		url = get_url()        
-		file_extension = re.search("([^.]*)$",url).group(1).lower()   
-	print("perrito") 
-	return url
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''    
+    while file_extension not in allowed_extension:        
+        url = get_url()        
+        file_extension = re.search("([^.]*)$",url).group(1).lower()   
+    print("perrito") 
+    return url
 
 def bop(bot, update):
-	url = image_url()
-	chat_id = update.message.chat_id
-	bot.send_photo(chat_id=chat_id, photo=url)
+    url = image_url()
+    print("encontre")
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
 
 
 
 def main():
-	print("Starting bot")
-	updater = Updater('805853693:AAHdOhCLO_ajPvEQ1K24VtpGkNcPb84ncfc')
-	dp = updater.dispatcher
-	dp.add_handler(CommandHandler('bop',bop))
-	updater.start_polling()
-	updater.idle()
+    print("Starting bot")
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    updater = Updater('805853693:AAHdOhCLO_ajPvEQ1K24VtpGkNcPb84ncfc')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    print("Starting start_polling")
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__': 
-	main()
+    main()
